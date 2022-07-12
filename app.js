0e70e2758d44229ae65a9132462c3bcffa238f43
duonglamh1999 <duonglamh1999@gmail.com>
2022-07-12 02:29:35 -0500

    Add Style, Add delete

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let form = document.querySelector('#form');
 let memeSection=document.querySelector('#meme-section-container')
-let linkInput = document.querySelector('#link');
-let topInput = document.querySelector('#top');
-let bottomInput = document.querySelector('#bottom');
+let linkInput = document.querySelector('#linkInput');
+let topInput = document.querySelector('#topInput');
+let bottomInput = document.querySelector('#bottomInput');
 form.addEventListener('submit', e=> {
     e.preventDefault();
     createMeme(linkInput.value,topInput.value,bottomInput.value);
@@ -11,24 +11,35 @@ form.addEventListener('submit', e=> {
     bottomInput.value = "";
   })
 
-memeSection.addEventListener('click', e => {
-  console.log(e.target);
-})
+memeSection.addEventListener('click', deleteMeme);
 
-function createMeme (link,top,bottom){ 
+
+function createMeme (link,top,bottom)
+{ 
 let memeContainer = document.createElement('div');
 memeContainer.setAttribute('class','meme-container');
 let memeImage = document.createElement('img');
-memeImage.setAttribute('alt',"It is a meme, laugh");
+memeImage.setAttribute('alt',"It is a meme, laugh!!!");
 memeImage.setAttribute('src',link);
 let wordContainer = document.createElement('div');
 wordContainer.setAttribute('class','word-container');
-let topText = document.createElement('h1');
-topText.setAttribute('class','top');
+let topText = document.createElement('p');
 topText.innerText= top;
-let bottomText = document.createElement('h1');
-bottomText.setAttribute('class','bottom');
+let bottomText = document.createElement('p');
 bottomText.innerText= bottom;
+let overlay =`<div class="overlay">
+<div class="overlay-text">Click to delete</div>
+</div>`;
 wordContainer.append(topText,bottomText);
-memeContainer.append(wordContainer,memeImage);
-memeSection.append(memeContainer)}
+memeContainer.append(memeImage,wordContainer);
+memeSection.append(memeContainer);
+memeContainer.innerHTML+=overlay;
+}
+function deleteMeme (e)
+{
+  let selector = e.target;
+  if(selector.className=='overlay-text')
+  {
+    selector.parentElement.parentElement.remove();
+  }
+}
